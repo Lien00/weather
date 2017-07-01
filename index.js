b670723f972f10980bce45ad4151f35a82be2d71
Lien00 <lollipop084191@gmail.com>
2017-07-01 21:00:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = require('./lib/linebot');
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1513395995,
+  channelSecret: 08370757249459a0801e242c12cdf2b9,
+  channelAccessToken: SJBIslPg49g1fda/RaK/uzzsVOiKpE4D/efEF32TaxGGZWMVWPKsKNIjZeT+BxmQZ2c0TMAMfdNiwKbSMSvp+L/TyZDJAvC/vDasijO8HeSjQAp3bhzP0sz0vzc3QnC5oKCm/7QVi+feix1AgmX9jgdB04t89/1O/w1cDnyilFU=
+});
